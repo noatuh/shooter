94e2eab2622c62d78e7035209813063d313659bf
noatuh <noatuh@outlook.com>
2023-12-21 23:01:44 -0500

    Setup targeting system

--- Assets/Scripts/Enemy/Target.cs
+++ Assets/Scripts/Enemy/Target.cs
@@ -1,18 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float health = 100f;
+
+    public void TakeDamage (float amount) 
     {
-        
+        health -= amount;
+        if (health <= 0f)
+        {
+            Die();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void Die ()
     {
-        
+        Destroy(gameObject);
     }
+
+
+
+
+
+
 }
--- Assets/Scripts/Rifle.cs
+++ Assets/Scripts/Rifle.cs
@@ -22,6 +22,12 @@ public class Rifle : MonoBehaviour
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
             Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
         }
     }
 
