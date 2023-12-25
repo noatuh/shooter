bc6afe4e6be2aa2b34498b3a9077baba09b197d5
noatuh <noatuh@outlook.com>
2023-12-25 01:01:58 -0500

    fixed huge patrolling AI issue (for now)

--- Assets/Scripts/Enemy/EnemyAI.cs
+++ Assets/Scripts/Enemy/EnemyAI.cs
@@ -5,64 +5,70 @@ public class EnemyAI : MonoBehaviour
     public float moveSpeed = 2.0f;
     public float chaseDistance = 5.0f;
     public float attackDistance = 1.5f;
-    public float attackDamage = 10.0f; // Public so you can modify it later in the Inspector
-    public float attackCooldown = 5.0f; // Time in seconds between attacks
+    public float attackDamage = 10.0f;
+    public float attackCooldown = 1.0f;
+    public float patrolDistance = 10.0f; // Distance each enemy can move from its start position
+    public float initialChaseDelay = 10f; // Delay before starting to chase
 
     private Transform playerTransform;
     private Vector3 randomDestination;
+    private Vector3 startPosition;
     private float changeDestinationTime = 5.0f;
     private float changeDestinationTimer;
     private float attackTimer;
+    private float chaseTimer;
 
     void Start()
     {
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        startPosition = transform.position; // Store the starting position
         SetRandomDestination();
+        chaseTimer = initialChaseDelay; // Initialize chase timer
     }
 
     void Update()
     {
         float distanceToPlayer = Vector3.Distance(transform.position, playerTransform.position);
 
-        if (distanceToPlayer <= attackDistance)
+        if (chaseTimer > 0)
         {
-            if (attackTimer <= 0)
-            {
-                AttackPlayer();
-                attackTimer = attackCooldown; // Reset the attack timer
-            }
-            else
-            {
-                attackTimer -= Time.deltaTime; // Decrease timer
-            }
-        }
-        else if (distanceToPlayer <= chaseDistance)
-        {
-            ChasePlayer();
+            chaseTimer -= Time.deltaTime;
         }
         else
         {
-            MoveRandomly();
+            if (distanceToPlayer <= attackDistance)
+            {
+                if (attackTimer <= 0)
+                {
+                    AttackPlayer();
+                    attackTimer = attackCooldown;
+                }
+                else
+                {
+                    attackTimer -= Time.deltaTime;
+                }
+            }
+            else if (distanceToPlayer <= chaseDistance)
+            {
+                ChasePlayer();
+            }
+            else
+            {
+                MoveRandomly();
+            }
         }
     }
 
     void ChasePlayer()
     {
+        Vector3 direction = (playerTransform.position - transform.position).normalized;
         transform.LookAt(playerTransform);
-        transform.position = Vector3.MoveTowards(transform.position, playerTransform.position, moveSpeed * Time.deltaTime);
+        transform.position += direction * moveSpeed * Time.deltaTime; // Directly setting the position
     }
 
     void MoveRandomly()
     {
         transform.position = Vector3.MoveTowards(transform.position, randomDestination, moveSpeed * Time.deltaTime);
-
-        // Calculate the new direction to look at
-        Vector3 direction = (randomDestination - transform.position).normalized;
-        if (direction != Vector3.zero)
-        {
-            Quaternion lookRotation = Quaternion.LookRotation(direction);
-            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5.0f); // Adjust the rotation speed if necessary
-        }
 
         if (Vector3.Distance(transform.position, randomDestination) < 0.5f || changeDestinationTimer > changeDestinationTime)
         {
@@ -75,14 +81,14 @@ public class EnemyAI : MonoBehaviour
 
     void SetRandomDestination()
     {
-        float randomX = Random.Range(-10.0f, 10.0f);
-        float randomZ = Random.Range(-10.0f, 10.0f);
-        randomDestination = new Vector3(randomX, transform.position.y, randomZ);
+        // Generate a random destination within a "patrolDistance" radius from the starting position
+        float randomX = Random.Range(startPosition.x - patrolDistance, startPosition.x + patrolDistance);
+        float randomZ = Random.Range(startPosition.z - patrolDistance, startPosition.z + patrolDistance);
+        randomDestination = new Vector3(randomX, startPosition.y, randomZ);
     }
 
     void AttackPlayer()
     {
-        // Assuming the player has a script named PlayerHealth attached to it
         PlayerHealth playerHealth = playerTransform.GetComponent<PlayerHealth>();
         if (playerHealth != null)
         {
