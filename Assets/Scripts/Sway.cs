aa2a479b8bc023f207ca6493dbce80bc10838fe2
noatuh <noatuh@outlook.com>
2023-12-30 06:29:38 -0500

    added weapon sway

--- Assets/Scripts/Sway.cs
+++ Assets/Scripts/Sway.cs
@@ -1,18 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Sway : MonoBehaviour
 {
+    public float amount = 0.02f;
+    public float maxAmount = 0.1f;
+    public float smoothAmount = 4.0f;
+
+    private Vector3 initialPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        initialPosition = transform.localPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float movementX = -Input.GetAxis("Mouse X") * amount;
+        float movementY = -Input.GetAxis("Mouse Y") * amount;
+        movementX = Mathf.Clamp(movementX, -maxAmount, maxAmount);
+        movementY = Mathf.Clamp(movementY, -maxAmount, maxAmount);
+        Vector3 finalPosition = new Vector3(movementX, movementY, 0);
+        transform.localPosition = Vector3.Lerp(transform.localPosition, finalPosition + initialPosition, Time.deltaTime * smoothAmount);
     }
 }
